67c3a928b3ee1cbc8fcf17606fe9727694c9d279
ManuellaBottega <manuelladasnevesbottega@gmail.com>
2025-08-31 06:38:53 -0300

    adicionando os outros arquivos

---
+++ queimada.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function(){
+    const armazenaCanvas = this.document.getElementById('canvas')
+    const contexto = canvas.getContext('2d')
+    canvas.width = 300
+    canvas.height = 169
+
+    class InputHandler {
+        
+    }
+})
