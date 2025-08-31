b1f9f8b2cead4bd8fb3ce685d4a3ce5bb3238882
ManuellaBottega <manuelladasnevesbottega@gmail.com>
2025-08-31 09:00:59 -0300

    começando js

--- queimada.js
+++ queimada.js
@@ -1,10 +1,14 @@
-window.addEventListener('load', function(){
-    const armazenaCanvas = this.document.getElementById('canvas')
-    const contexto = canvas.getContext('2d')
-    canvas.width = 300
-    canvas.height = 169
+//Update é onde move os personagens, verifica colisoes, pontuação, qualquer coisa envolvendo logica
+//Draw é oque tá aparecendo na tela no momento
 
-    class InputHandler {
-        
-    }
+window.addEventListener('load', function(){
+//o load garante que tudo do html e css carregou antes de iniciar o codigo
+//o window cria uma pagina e addAdventListener adciona um evento a pagina, nesse caso load
+    const armazenaCanvas = this.document.getElementById('canvas')
+//procura a variavel canvas e armazena
+    const contexto = canvas.getContext('2d')
+//o 2d permite desenhar imagem texto e tal no canvas, contexto tem todas as ferramentas para isso
+    canvas.width = 1000   
+    canvas.height = 500
+//define a area de desenho
 })
